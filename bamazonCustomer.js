c08e851d611d6c9a9e375f5b8ae209fa366c29bc
Astrid <leigh.stevens.ls@gmail.com>
2019-04-16 04:23:14 -0500

    working complete app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
-var inquierer = require ("inquirer");
-var mysql = require ("mysql");
+var inquirer = require("inquirer");
+var mysql = require("mysql");
 
-var connection = mysql.createConnection ({
+var connection = mysql.createConnection({
 
     host: "localhost",
 
@@ -15,7 +15,7 @@ var connection = mysql.createConnection ({
 
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadID + "\n");
 
@@ -24,60 +24,74 @@ connection.connect(function(err) {
 
 function displayProducts() {
     console.log("Here are our products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
 
         if (err) throw err;
-        console.log(res);
-        
-        connection.end();
+        console.table(res);
+
+        start(res);
+        // connection.end();
 
     });
 }
 
 
-function start() {
-    inquierer
-        .prompt({
+function start(results) {
+    inquirer
+        .prompt([
+            {
             name: "idOfProduct",
-            type: "input",
+            type: "number",
             message: "What is the ID of the product you would like to buy?",
-        })
-            .then(function(answer) {
-                var chosenItem;
-
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].product_name === answer.choice) {
-
-                        chosenItem = results[i];
-
-                    }
-                }
-            
-            if (chosenItem.Item.quantity > parseInt(answer.stock_quantity);
-            connection.query (
-
-                "UPDATE products SET ? WHERE ?",
-
-                [ {
-                    quantity: answer.stock_quantity
-                },
-                {
-                    item_id: chosenItem.id
-                }
-            ],
-            function(error) {
-                if error throw err;
-                console.log("Item ordered successfully!");
-
-                start();
-
-            }
-        else {
-            console.log("That item is out of stock, unfortunatley.  Please choose another item.");
-            start();
+        },
+        {
+            name: "stock_quantity",
+            type: "number",
+            message: "What is the quantity of the product you would like to buy?",
         }
-                }]
-            )
+    ])
+        .then(function (answer) {
+            var chosenItem;
+        
+            for (var i = 0; i < results.length; i++) {
+                //console.log(results[i].item_id);
+                if (results[i].item_id === answer.idOfProduct) {
+                    
+                    chosenItem = results[i];
 
+                }
+            }
+            //console.log(results);
+            console.table(chosenItem);
+            console.log(chosenItem.stock_quantity);
+            console.log(answer.stock_quantity);
+            var newQuantity = chosenItem.stock_quantity - answer.stock_quantity;
+            if (chosenItem.stock_quantity >= answer.stock_quantity) {
+                connection.query(
+
+                    "UPDATE products SET ? WHERE ?",
+
+                    [{
+                        stock_quantity: newQuantity
+                    },
+                    {
+                        item_id: chosenItem.id
+                    }
+                    ],
+                    function (error) {
+                        if (error) throw err;
+                        console.log("Item ordered successfully!");
+
+                        start();
+
+                    })
+            }
+            else {
+                console.log("That item is out of stock, unfortunatley.  Please choose another item.");
+                start();
+            }
         })
 }
+
+
+
